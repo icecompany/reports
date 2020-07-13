f0ff3cc91bf0b279793469aa4fa8c26126dfebb3
Anton <asharikov@icecompany.org>
2020-07-13 20:23:18 +0300

    Version 1.2.0

--- components/com_reports/views/cron/tmpl/default.php
+++ components/com_reports/views/cron/tmpl/default.php
@@ -1,17 +1,2 @@
 <?php
 defined('_JEXEC') or die;
-
-$app = JFactory::getApplication('administrator');
-jimport('joomla.user.helper');
-$options = array('remember' => true);
-if (JFactory::getApplication()->login(['username' => 'admin', 'password' => '44dupyx579'])) {
-    JTable::addIncludePath(JPATH_COMPONENT_ADMINISTRATOR . "/tables");
-    JModelLegacy::addIncludePath(JPATH_COMPONENT_ADMINISTRATOR . "/models", "ReportsModel");
-    $model = JModelLegacy::getInstance("Reports", "ReportsModel", ['cron' => true]);
-    $items = $model->getItems();
-    if (empty($items['items'])) die();
-    exit(var_dump(ReportsHelper::getConfig('username')));
-    die();
-} else {
-    echo "error";
-}
